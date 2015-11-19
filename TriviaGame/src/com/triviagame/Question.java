4d5babff98fb3db07e9dd927c8a0958b037c7c11
Alexander Scanlan <ajscanlan27@gmail.com>
2015-11-19 10:04:27 +0000

    init commit question

---
+++ TriviaGame/src/com/triviagame/Question.java
@@ -0,0 +1,9 @@
+package com.triviagame;
+
+import java.util.HashMap;
+
+public class Question {
+	private String question;
+	private HashMap<String, Boolean> answers;
+	//private Player questioneer;
+}
