c83032544230ae8fe26ae71d04d406668f6a1c4f
PrzemekTGK <PrzemeTGK@yahoo.com>
2015-11-19 10:40:41 +0000

    next ui commit

--- TriviaGame/src/com/triviagame/ui/Trivia.java
+++ TriviaGame/src/com/triviagame/ui/Trivia.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -25,19 +26,22 @@ public class Trivia extends Application {
     public void start(Stage stage) {
        
         Label title = new Label("Trivia");
+        title.setStyle("-fx-font-size: 50; -fx-text-alignment: center");
         Label numPlayersText = new Label ("Select the number of players");
-        ComboBox numPlayers = new ComboBox();
-        
-        ObservableList<String> numPlayersOptions = 
+        ComboBox numPlayersOptions = new ComboBox();
+        ObservableList<String> numPlayer = 
                 FXCollections.observableArrayList("1", "2", "3", "4");
-        
+        numPlayersOptions.setItems(numPlayer);
         Button btn = new Button();
         btn.setText("New Game");
         
+        VBox vbox = new VBox();
         
-        Scene scene = new Scene(new Group());
+        vbox.getChildren().addAll(title, numPlayersText, numPlayersOptions, btn);
         
-        stage.setTitle(title.toString());
+        Scene scene = new Scene(vbox, 500, 500);
+        
+        stage.setTitle(title.getText());
         stage.setScene(scene);
         stage.show();
     }
