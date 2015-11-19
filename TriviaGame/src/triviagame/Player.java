12e1af6cde672c104cbb2cc6fe28392152aebeb5
Conor Brennan <conor.90@hotmail.com>
2015-11-19 10:05:44 +0000

    Added player class

---
+++ TriviaGame/src/triviagame/Player.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triviagame;
+
+/**
+ *
+ * @author User1
+ */
+public class Player {
+    private String name ;
+    private int score;
+    private int questionsAnswered;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getQuestionsAnswered() {
+        return questionsAnswered;
+    }
+
+    public void setQuestionsAnswered(int questionsAnswered) {
+        this.questionsAnswered = questionsAnswered;
+    }
+    
+    
+}
